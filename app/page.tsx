1aa64196b068d55cc63b4ebbff1a475f95fb2b4b
testatilde <aaltismann@gmail.com>
2025-05-22 16:13:53 +0000

    hh

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ export const revalidate = 10; // seconds
 type PageType = {
   title: string;
   slug: string;
-  publishedAt: string;
   body: {
     raw: any;
   };
@@ -18,10 +17,9 @@ async function getPages() {
     },
     body: JSON.stringify({
       query: `query Pages {
-        pages(orderBy: publishedAt_DESC) {
+        pages(orderBy: title_DESC) {
           title
           slug
-          publishedAt
           body {
             raw
           }
@@ -38,16 +36,16 @@ export default async function Page() {
   
   return (
     <>
-      <header className="static">
+      <header className="fixed top-0 left-0 right-0 z-50">
         <div className="bg-gray-300 text-white py-1">
           <div className="container mx-auto px-4">
             <div className="flex items-center justify-center space-x-6">
               <h1 className="text-3xl font-bold">~</h1>
               <h1 className="text-3xl font-bold">Tilde</h1>
               <nav className="flex flex-row gap-4">
-                <a href="https://www.svtplay.se/kanaler" target="_blank" className="hover:text-black transition-colors duration-200 transform hover:scale-150 transition-transform duration-300">SVT PLAY</a>
-                <a href="https://embed.ted.com" target="_blank" className="hover:text-black transition-colors duration-200 transform hover:scale-150 transition-transform duration-300">TED</a>
-                <a href="https://www.arte.tv" target="_blank" className="hover:text-black transition-colors duration-200 transform hover:scale-150 transition-transform duration-300">ARTE</a>
+                <a href="https://www.svtplay.se/kanaler" className="hover:text-black transition-colors duration-200 transform hover:scale-150 transition-transform duration-300">SVT PLAY</a>
+                <a href="https://embed.ted.com" className="hover:text-black transition-colors duration-200 transform hover:scale-150 transition-transform duration-300">TED</a>
+                <a href="https://www.arte.tv" className="hover:text-black transition-colors duration-200 transform hover:scale-150 transition-transform duration-300">ARTE</a>
               </nav>
             </div>
           </div>
@@ -70,9 +68,9 @@ export default async function Page() {
         </div>
       </header>
 
-      <main className="container mx-auto px-4 sm:px-6 md:px-8 lg:px-16 xl:px-32 mt-0 mb-32">
+      <main className="container mx-auto px-32 mt-0 mb-32">
         <h1 className="text-4xl font-bold mb-8"></h1>
-        <div className="grid gap-32 md:gap-48 lg:gap-64">
+        <div className="grid gap-64">
           {pages.map((page: PageType) => (
             <div id={page.slug} key={page.slug} className="bg-black p-0 scroll-mt-0">
               <h2 className="text-4xl uppercase text-white text-right mb-1 p-1">{page.title}</h2>
@@ -83,14 +81,6 @@ export default async function Page() {
           ))}
         </div>
       </main>
-
-      <a 
-        href="#"
-        className="fixed bottom-8 right-8 bg-white text-black px-4 py-2 rounded-full flex items-center gap-2 hover:bg-gray-200 transition-colors duration-200"
-      >
-        <span className="text-2xl">↑</span>
-        <span>Menu</span>
-      </a>
     </>
   );
 }
